9c8f6e9b082b72d2c01aee934db194c8480b4193
alihamidi1387git-prog@gmail.com <ali.hamidi1401400@gmail.com>
2026-09-22 21:16:08 +0330

    refactor:reorganize project structure

--- interfaces/ICustomNFTReceiver.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-
-interface ICustomNFTReceiver {
-    function onCustomNFTReceived(address operator, address from, uint tokenId, bytes calldata data) external returns (bytes4);
-}
